a199a61d7ac4becb0d788e8548b5d56023ac2a1e
LinnierGames <esericksanc@gmail.com>
2020-07-02 12:30:48 -0700

    inject Weather mock into Agenda tests

--- SuperCoolProject/Services/AgendaService/Impl/Tests/AgendaServiceTests.swift
+++ SuperCoolProject/Services/AgendaService/Impl/Tests/AgendaServiceTests.swift
@@ -8,18 +8,22 @@
 
 import XCTest
 import CalendarServiceMocks
+import WeatherServiceMocks
 
 @testable import AgendaService
 
 class AgendaServiceTests: XCTestCase {
   var calendarServiceMock: CalendarServiceMock!
+  var weatherServiceMock: WeatherServiceMock!
 
   override func setUp() {
     super.setUp()
     calendarServiceMock = CalendarServiceMock(testCase: self)
+    weatherServiceMock = WeatherServiceMock(testCase: self)
   }
 
   func testOne() {
-    let service = AgendaServiceImpl(calendarService: calendarServiceMock, weatherService: <#T##WeatherService#>)
+    let service = AgendaServiceImpl(
+      calendarService: calendarServiceMock, weatherService: weatherServiceMock)
   }
 }
